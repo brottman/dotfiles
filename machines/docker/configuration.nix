d4e979aa2afea8e5b2b1c962eedd0a4f8d135d3f
Brian <your-email@example.com>
2025-11-22 15:35:31 -0500

    asdfadsf

--- machines/docker/configuration.nix
+++ machines/docker/configuration.nix
@@ -65,7 +65,7 @@
     allowedTCPPorts = [ 
                                           22 # ssh
                                           80 # npm
-                                          81 # npm
+                                          #81 # npm
                                           443 # npm
                                           8000 # django
                                           9000 # portainer
